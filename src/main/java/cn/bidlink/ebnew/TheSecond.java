da65ab2f31f2a876926bc0af1b3f7a2a3da1fc53
wanghaijiao <1293177288@qq.com>
2018-01-05 17:30:42 +0800

    用户类型

--- src/main/java/cn/bidlink/ebnew/TheSecond.java
+++ src/main/java/cn/bidlink/ebnew/TheSecond.java
@@ -1,4 +1,13 @@
 package cn.bidlink.ebnew;
 
 public class TheSecond {
+    private String userName;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
 }
