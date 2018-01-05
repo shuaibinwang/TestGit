6c01e80afa255d6a5ec2d2d28e7a30d57e3fede3
wangshuaibin <1006370948@qq.com>
2018-01-05 17:13:15 +0800

    添加年龄

--- src/main/java/cn/bidlink/ebnew/TheFirst.java
+++ src/main/java/cn/bidlink/ebnew/TheFirst.java
@@ -1,4 +1,13 @@
 package cn.bidlink.ebnew;
 
 public class TheFirst {
+    private Integer age;
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
 }
