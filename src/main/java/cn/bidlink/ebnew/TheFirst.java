312c5a55bc35417e65df526cb6a37299c6b50378
wangshuaibin <1006370948@qq.com>
2018-01-05 16:56:18 +0800

    添加姓名

--- src/main/java/cn/bidlink/ebnew/TheFirst.java
+++ src/main/java/cn/bidlink/ebnew/TheFirst.java
@@ -1,4 +1,14 @@
 package cn.bidlink.ebnew;
 
 public class TheFirst {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
