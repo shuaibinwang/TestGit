096e33bbfbdc98b09e7a3135110a133c05bb1f50
wangshuaibin <testqq.com>
2018-01-08 16:39:36 +0800

    Merge remote-tracking branches 'remotes/origin/wangshuaibin_branch' and 'remotes/origin/wsb'

--- src/main/java/cn/bidlink/ebnew/People.java
+++ src/main/java/cn/bidlink/ebnew/People.java
@@ -3,7 +3,18 @@ package cn.bidlink.ebnew;
 public class People {
     private String name;
 
+    private  String weight;
+
     private Integer age;
+
+    public String getWeight() {
+        return weight;
+    }
+
+    public void setWeight(String weight) {
+        this.weight = weight;
+    }
+
 
     public Integer getAge() {
         return age;
