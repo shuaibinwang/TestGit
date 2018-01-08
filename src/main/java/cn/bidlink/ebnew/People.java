8e7d3fabc17897052f5e2dfb16e79e0d1141a392
wangshuaibin <testqq.com>
2018-01-08 16:20:23 +0800

    添加人类

---
+++ src/main/java/cn/bidlink/ebnew/People.java
@@ -0,0 +1,13 @@
+package cn.bidlink.ebnew;
+
+public class People {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
