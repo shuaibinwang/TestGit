3f25a7eb1300b3ca51317a5e82ee42d3e7084dc6
wangshuaibin <testqq.com>
2018-01-08 16:30:10 +0800

    添加年龄

--- src/main/java/cn/bidlink/ebnew/People.java
+++ src/main/java/cn/bidlink/ebnew/People.java
@@ -3,6 +3,16 @@ package cn.bidlink.ebnew;
 public class People {
     private String name;
 
+    private Integer age;
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
     public String getName() {
         return name;
     }
