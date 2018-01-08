ffefd813d33781b8565c406eba1f383df2472f7e
wangshuaibin <testqq.com>
2018-01-08 16:31:09 +0800

    添加体重

--- src/main/java/cn/bidlink/ebnew/People.java
+++ src/main/java/cn/bidlink/ebnew/People.java
@@ -3,6 +3,16 @@ package cn.bidlink.ebnew;
 public class People {
     private String name;
 
+    private  String weight;
+
+    public String getWeight() {
+        return weight;
+    }
+
+    public void setWeight(String weight) {
+        this.weight = weight;
+    }
+
     public String getName() {
         return name;
     }
